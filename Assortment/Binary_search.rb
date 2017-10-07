3581d9fb3a5df70bf37245ef2460bd1b40804b46
Zuhair A. Shaikh <zuhair.a.shaikh@outlook.com>
2017-10-06 22:02:49 -0700

    Complete binary search

---
+++ Assortment/Binary_search.rb
@@ -0,0 +1,33 @@
+def bs_iter(arr, target)
+  return arr.first if arr.length < 2
+
+  start_idx, end_idx = 0, arr.length - 1
+
+  while start_idx <= end_idx
+    mid = (start_idx + end_idx) / 2
+    if arr[mid] == target
+      return mid
+    elsif arr[mid] > target
+      end_idx = mid - 1
+    else start_idx = mid + 1
+    end
+  end
+  nil
+end
+
+def bs_rec(arr, target)
+  return nil if arr.empty?
+
+  mid = arr.length / 2
+
+  if arr[mid] == target
+    return mid
+  elsif arr[mid] > target
+    bs_rec(arr[0...mid], target)
+  else
+    mid + bs_rec(arr[mid + 1..-1], target) + 1
+  end
+end
+
+p bs_iter([2, 3, 4, 10, 40], 10)
+p bs_rec([2, 3, 4, 10, 40], 1)
